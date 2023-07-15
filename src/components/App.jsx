f1b0bd2a1b251dfa5e0465661d8e1dc29a388320
Michael Nascimento <michael.nsc@outlook.com>
2023-07-15 19:49:26 -0300

    live-coding 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,51 @@
+import { useState } from "react";
 import Header from "./Header";
 import Text from "./Text";
+import TextInput from "./TextInput";
 
 function App() {
-  // POSSO RETORNAR:
-  // strings
-  // arrays
-  // outros componentes
-  // jsx
-  // null ou undefined
-  // boolean
-  // numbers
+  // RENDERIZAÇÃO E RE-RENDERIZAÇÃO
 
-  // NÃO POSSO RETORNAR
-  // objetos
+  // SEMMMMMMMMPRE, SEEEEEEEEEEEEEEEEEEMPRE
+  // retorna um array com dois negócios:
+  // [0] -> O VALOR DO ESTADO
+  // [1] -> UMA FUNÇÃO QUE ALTERA O VALOR DO ESTADO
+
+  // const state1 = useState("");
+  // const numeroDaConta = state1[0];
+  // const setNumeroDaConta = state1[1];
+  const [numeroDaConta, setNumeroDaConta] = useState("");
+
+  const state2 = useState("");
+  const senha = state2[0];
+  const setSenha = state2[1];
+
+  console.log(senha);
+
+  // const listaDeFrutas = ['maçã', 'banana', 'pera']
+  // const [fruta1, fruta2, fruta3] = listaDeFrutas
 
   return (
     <div>
       <Header />
-      <Text element={"h3"}>Olá, mundo!!!!!!!!!!!!!</Text>
+      <Text element={"h1"}>Número da conta: {numeroDaConta}</Text>
+      <TextInput
+        onChangeText={setNumeroDaConta}
+        label="Número da conta com dígito"
+        type="text"
+      />
+      <TextInput onChangeText={setSenha} label="Senha" type="password" />
     </div>
   );
 }
 
 export default App;
+
+/*
+  const display = document.querySelector("h1")
+  const input = document.querySelector("input")
+
+  document.addEventListener("change", function(event) {
+    display.innerText = event.target.value
+  })
+*/
