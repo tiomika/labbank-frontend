4876500aaeb23810eec39b48a67be76eb1b2efa1
Michael Nascimento <michael.nsc@outlook.com>
2023-07-22 19:15:48 -0300

    extrai pagina de Login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import WalletIllustration from "../../assets/imgs/wallet.svg";
+
+import { Button } from "../../components/Button";
+import { Paper } from "../../components/Paper";
+import { Text } from "../../components/Text";
+import { TextInput } from "../../components/TextInput";
+
+function Login() {
+  const [numeroDaConta, setNumeroDaConta] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <div className="formContainer">
+      <Paper>
+        <Text element={"h1"}>Seja bem vindo!</Text>
+        <div className="subtitle">
+          <Text element={"paragraph"}>Digite os seus dados de acesso</Text>
+        </div>
+        <div className="formContent">
+          <div>
+            <TextInput
+              onChangeText={setNumeroDaConta}
+              label="Número da conta com dígito"
+              type="text"
+            />
+            <TextInput onChangeText={setSenha} label="Senha" type="password" />
+            {/* <Link to="#">Esqueci minha senha</Link> */}
+            <Button>Entrar</Button>
+          </div>
+          <div>
+            <img src={WalletIllustration} alt="Wallet at login page" />
+          </div>
+        </div>
+        {/* <DebugState state={{ numeroDaConta, senha }} /> */}
+      </Paper>
+    </div>
+  );
+}
+
+export default Login;
