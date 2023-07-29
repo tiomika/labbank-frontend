1dc2407c6673c51a441896827bbedc97aca5a792
Michael Nascimento <michael.nsc@outlook.com>
2023-07-29 16:12:36 -0300

    atualiza context do App

--- src/contexts/AppContext/AppContext.jsx
+++ src/contexts/AppContext/AppContext.jsx
@@ -1,6 +1,11 @@
 import { createContext } from "react";
 
-export const appContext = createContext();
+export const appContext = createContext({
+  valores: { numeroDaConta: "", senha: "" },
+  mostrarValores: () => {},
+  setNumeroDaConta: () => {},
+  setSenha: () => {},
+});
 
 const AppContext = appContext.Provider;
 
