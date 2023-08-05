5f76e595dad319fef61820550349a4d36c3776e4
Michael Nascimento <michael.nsc@outlook.com>
2023-08-05 17:51:52 -0300

    componente Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+
+import { SessionHeader } from "../../components/SessionHeader"
+import "./Home.css"
+
+
+function Home() {
+  return (
+    <>
+      <SessionHeader />
+      <main>
+        <div className="under_construction">
+          Em construção...
+        </div>
+      </main>
+    </>
+  )
+
+
+}
+
+export default Home
