7e496ec4b7999573cc7dee24beeefc3e109b0add
Luis Federico Vilar Velazquez <124309725+Luis-Vilar@users.noreply.github.com>
2023-08-21 18:59:05 -0300

    sugestões aceitas..
    Co-authored-by: Michael Nascimento <35317401+mikansc@users.noreply.github.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <header className={`${styles.header}`}>
+    <header className={styles.header}>
       <Container>
         <Logo />
       </Container>
